eb8f496ca21a453989676280293ae3f2fdf33915
PedroWernek <pedrowcnetto@gmail.com>
2025-03-26 19:32:13 -0300

    first commit

---
+++ Controllers/PersonagemController.cs
@@ -0,0 +1,11 @@
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProjetoDBZ.Controllers
+{
+    [Route("api/[controller]")]
+    public class PersonagemController : ControllerBase
+    {
+        
+    }
+}
