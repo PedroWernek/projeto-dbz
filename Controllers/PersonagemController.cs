d8c9bb786e93af243264668efe9e408adaaeca35
PedroWernek <pedrowcnetto@gmail.com>
2025-03-26 20:29:50 -0300

    tfinish

--- Controllers/PersonagemController.cs
+++ Controllers/PersonagemController.cs
@@ -1,11 +1,17 @@
-
 using Microsoft.AspNetCore.Mvc;
+using ProjetoDBZ.Models;
 
 namespace ProjetoDBZ.Controllers
 {
+    [ApiController]
     [Route("api/[controller]")]
     public class PersonagemController : ControllerBase
     {
+        private readonly AppDbContext _appDbContext;
+        public PersonagemController(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        } 
         
     }
 }
